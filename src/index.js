dbbb23ae27aa5400edd1397a2e519dd0a30f767e
Sam Hadfield <sthtech@protonmail.com>
2025-05-13 17:54:19 -0500

    Add callPage() function
    Set initial page load to home() to call the home page. Add click
    event listeners to buttons which will call callPage() function upon
    hearing a 'click'. This will wipe the #content div of its current content
    then call the function that corresponds with the button click i.e.
    'Menu', 'Contact', or back to 'Home'.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import "./styles.css";
 import { home } from "./home";
 import { menu } from "./menu";
 import { contact } from "./contact";
-// home();
-// menu();
-contact();
+
+// Initial Page Load
+home();
+
+const contentDiv = document.querySelector("#content");
+
+const callPage = function (button) {
+  contentDiv.innerHTML = "";
+
+  if (button === "Home") {
+    home();
+  } else if (button === "Menu") {
+    menu();
+  } else if (button === "Contact") {
+    contact();
+  }
+};
+
+const buttons = Array.from(document.querySelectorAll(".button"));
+
+buttons.forEach((button) => {
+  const buttonContents = button.innerText;
+  button.addEventListener("click", () => callPage(buttonContents));
+});

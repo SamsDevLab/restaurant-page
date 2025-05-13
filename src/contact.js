784a04525504dd7dfa54984608bb1901cf6cbf80
Sam Hadfield <sthtech@protonmail.com>
2025-05-13 16:38:09 -0500

    Refactor createContactElements function
    Refactor function to include both the creation of elements as well
    as appending content to the DOM. Originally had these duties split into
    two separate functions but this caused unneeded complexity.
    
    Move 'sections' object array above createContactElements function.

--- src/contact.js
+++ src/contact.js
@@ -1,26 +1,65 @@
 export const contact = function () {
   const contentDiv = document.querySelector("#content");
   const contactDiv = document.createElement("div");
-  contactDiv.classList.add("contact-div");
+  contactDiv.setAttribute("id", "contact-div");
+  contentDiv.appendChild(contactDiv);
 
-  function createContactElements() {
-    for (let i = 0; i < 2; i++) {
+  const sections = [
+    {
+      header: "About",
+      description:
+        "Established in 1987, Mudgut's Cafe is a desert diner inspired by the Mad Max franchise. Named after Mudguts, a member of Toecutter's gang of heathens, this desert oasis will leave you refreshed for your drive to the next town.",
+    },
+    {
+      header: "Contact",
+      addressHeader: "Address:",
+      addressLineOne: "555 Middle of Nowhere Dr.",
+      addressLineTwo: "Somewhere USA 55555",
+      phoneHeader: "Phone:",
+      phoneNumber: "555-555-5555",
+      emailHeader: "Email:",
+      email: "mudgutscafe@someemail.com",
+    },
+  ];
+
+  function createContactElements(sections) {
+    const contactSections = sections;
+
+    contactSections.forEach((element) => {
       const innerDiv = document.createElement("div");
       innerDiv.classList.add("inner-div");
 
-      const header = document.createElement("h2");
-      header.classList.add("header");
+      for (let [key, value] of Object.entries(element)) {
+        if (key === "header") {
+          const header = document.createElement("h2");
+          header.classList.add("headerDiv");
+          header.innerText = element.header;
+          innerDiv.appendChild(header);
+        } else if (key === "description") {
+          const paragraph = document.createElement("p");
+          paragraph.innerText = value;
+          innerDiv.appendChild(paragraph);
+        } else if (
+          key === "addressHeader" ||
+          key === "phoneHeader" ||
+          key === "emailHeader"
+        ) {
+          const header = document.createElement("h3");
+          header.innerText = value;
+          innerDiv.appendChild(header);
+        } else {
+          const paragraph = document.createElement("p");
+          paragraph.innerText = value;
+          innerDiv.appendChild(paragraph);
+        }
+      }
 
-      const description = document.createElement("p");
-      description.classList.add("description");
-
-      innerDiv.appendChild(header);
-      innerDiv.appendChild(description);
       contactDiv.appendChild(innerDiv);
-    }
+    });
+
+    const image = document.createElement("img");
+    contactDiv.appendChild(image);
   }
 
-  createContactElements();
-
-  contentDiv.appendChild(contactDiv);
+  createContactElements(sections);
 };
